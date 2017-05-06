536e7240a82957ac7c02b7234317ba96ccf610ea
Frederic Nowacki <nowacki.frederic@gmail.com>
2017-05-07 00:47:24 +0200

    Add layout and home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TopbarComponent } from './layout/topbar/topbar.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { BotbarComponent } from './layout/botbar/botbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TopbarComponent,
+    SidebarComponent,
+    BotbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/layout/botbar/botbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-botbar',
+  templateUrl: './botbar.component.html',
+  styleUrls: ['./botbar.component.scss']
+})
+export class BotbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
